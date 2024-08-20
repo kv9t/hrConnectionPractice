74c3dce74328ee968539c6493f7520008a77ab4b
kv <voorhees.kristen@icloud.com>
2024-08-20 14:37:47 -0700

    Initial Commit

---
+++ hrConnectionPractice/hrConnectionPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hrConnectionPracticeApp.swift
+//  hrConnectionPractice
+//
+//  Created by Kristen Voorhees on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct hrConnectionPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
